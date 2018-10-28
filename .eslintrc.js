867f7009f7f160b098dbd195679141f6d8a6ab97
David Alvarez <davidac0291@gmail.com>
2018-10-28 11:45:22 +0100

    Added Vuetify. Started to define the structure.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,22 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    jest: true
   },
   'extends': [
     'plugin:vue/essential',
-    '@vue/standard'
+    'eslint:recommended'
   ],
   rules: {
+    'indent': [2, 2],
+    'linebreak-style': [2, 'unix'],
+    'semi': [2, 'always'],
+    'camelcase': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
   }
-}
+};
